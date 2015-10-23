353e389d45075502e76995cbe33c878292c0619e
Bunmi <bunmie.esther@gmail.com>
2015-10-23 10:27:06 +0100

    updated files
    made new changes to the codes

--- Serials.php
+++ Serials.php
@@ -1,25 +1,51 @@
 <?php
+require_once "connection.php";
 class Serials{
-	private $id;
-	private $productId;
+	//private $manufacturerId;
+	//private $productId;
+	private $connection;
 	
-	public function __construct(){
-		$this->id = $id;
-		$this->productId = $productId;
+	public function __construct($connection){
+		//$this->manufacturerId = $manufacturerid;
+		//$this->productId = $productid;
+		$this->connection = $connection;
 	}
 	
 	//get and set functions
-	public function getId(){
-		return $this->id;
-	}
-	public function getProductId(){
-		return $this->productId;
-	}
-	public function setId($ID){
-		$this->id = $ID;
-	}
-	public function getProductId(){
-		return $this->productId;
-	}
+	// public function getManufacturerId(){
+		// return $this->manufacturerId;
+	// }
+	// public function getProductId(){
+		// return $this->productId;
+	// }
+	// public function setId($ID){
+		// $this->id = $ID;
+	// }
+	// public function getProductId(){
+		// return $this->productId;
+	// }
+	
+	
+	//generates random strings of characters based on the given length
+	public function randGenerator($length) {
+        mt_srand((double)microtime() * 1000000); 
+        $possible = 'abcdefghijklmnopqrstuvwxyz0123456789' . 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'; 
+        $randompass =""; 
+        while(strlen($randompass) < $length) {
+                $randompass .= substr($possible, mt_rand(0, strlen($possible) -1), 1); 
+        }
+		return($randompass);
+		}
+
+	public function getSerial($manufacturerId, $numberOfSerials){
+			for($i=0; $i<$numberOfSerials; $i++){
+				$randomString = self::randGenerator(8);
+				$productId = $manufacturerId.$randomString;	
+				
+				//saves manufacturerId and productId into the database
+				$query = "insert into serials values(null, '$manufacturerId', '$productId')";
+				$result = $this->connection->query($query) or die(mysqli_error($this->connection));
+			}
+		}
 }
 ?>
--- validate.php
+++ validate.php
@@ -1,29 +1,40 @@
 <?php 
-
-require_once"manufacturer.php";
+require_once "connection.php";
+require_once "manufacturer.php";
 require_once "manufacturerDao.php";
+require_once "Serials.php";
 
 if(isset($_POST["submit"])){
-	$prefix= $_POST["selected"];
-	$serial= $_POST["numofSerials"];
+	$manufacturerId= $_POST["selected"];
+	$numofSerials = $_POST["numofSerials"];
 
-	echo getSerial($prefix, $serial);
-	
+	$con = new connection();
+	$connection = $con->getConnection();
+	$serialObject = new Serials($connection);
+	$serialObject->getSerial($manufacturerId, $numofSerials);	
+	header("location:index.php");	
 }
+
 
 if(isset($_POST["create"])){
 	$name = $_POST["name"];
 	$manufacturer = new manufacturer($name);
 	$manufacturerDao = new manufacturerDao();
 	$manufacturerDao->create($manufacturer);
-	
+	header("location:index.php");	
 }
-		
-       function randGenerator($length) {
+
+
+
+
+
+
+		//generates random strings of characters based on the given length
+		function randGenerator($length) {
         mt_srand((double)microtime() * 1000000); 
         $possible = 'abcdefghijklmnopqrstuvwxyz0123456789' . 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'; 
         $randompass =""; 
-        while(strlen($randompass) < $length) { 
+        while(strlen($randompass) < $length) {
                 $randompass .= substr($possible, mt_rand(0, strlen($possible) -1), 1); 
         }
 		return($randompass);
@@ -32,6 +43,7 @@ if(isset($_POST["create"])){
 		function getSerial($prefix, $numberOfSerials){
 			for($i=0; $i<$numberOfSerials; $i++){
 				echo $prefix.randGenerator(8)."<br/>";
+				
 			}
 		}
 ?>
